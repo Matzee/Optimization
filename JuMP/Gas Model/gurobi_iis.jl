0fab70de7ff8522dbfa63edfaa30f4ad9db198cd
Matthias Zech <matthias.zech@gmail.com>
2017-10-04 00:43:13 +0200

    NLP with warm start

---
+++ JuMP/Gas Model/gurobi_iis.jl
@@ -0,0 +1,33 @@
+function print_iis_gurobi(m::Model)
+
+    grb = getrawsolver(m)
+    Gurobi.computeIIS(grb)
+    numconstr = Gurobi.num_constrs(grb)
+    numvar = Gurobi.num_vars(grb)
+
+    iisconstr = Gurobi.get_intattrarray(grb, "IISConstr", 1, numconstr)
+    iislb = Gurobi.get_intattrarray(grb, "IISLB", 1, numvar)
+    iisub = Gurobi.get_intattrarray(grb, "IISUB", 1, numvar)
+
+    println("Irreducible Inconsistent Subsystem (IIS)")
+    println("Variable bounds:")
+    for i in 1:numvar
+        v = Variable(m, i)
+        if iislb[i] != 0 && iisub[i] != 0
+            println(getlowerbound(v), " <= ", getname(v), " <= ", getupperbound(v))
+        elseif iislb[i] != 0
+            println(getname(v), " >= ", getlowerbound(v))
+        elseif iisub[i] != 0
+            println(getname(v), " <= ", getupperbound(v))
+        end
+    end
+
+
+    println("Constraints:")
+    for i in 1:numconstr
+        if iisconstr[i] != 0
+            println(ConstraintRef{Model,LinearConstraint}(m, i))
+        end
+    end
+    println("End of IIS")
+end
